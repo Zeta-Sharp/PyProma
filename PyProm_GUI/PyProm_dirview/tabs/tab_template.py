3c469e9a29dad0f48b046a2655f5ab04c4349ad9
rikeidanshi <rikeidanshi@duck.com>
2024-08-10 11:26:06 +0900

    made tabs module

---
+++ PyProm_GUI/PyProm_dirview/tabs/tab_template.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+from tkinter import Frame
+
+
+class TabTemplate(ABC, Frame):
+    def __init__(self, master=None):
+        super().__init__(master, width=800, height=600)
+        self.propagate(False)
+
+    @abstractmethod
+    def refresh(self):
+        pass
