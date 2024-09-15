92051000bc5713e9375c644c62741877c78d4bd5
rikeidanshi <rikeidanshi@duck.com>
2024-09-15 20:02:53 +0900

    renamed some files

--- PyProma_GUI/PyProma_GUI_main.py
+++ PyProma_GUI/PyProma_GUI_main.py
@@ -6,4 +6,4 @@ from PyProma_project_view import PyProma_project_view
 if __name__ == "__main__":
     script_path = Path(__file__).resolve().parent.parent
     os.chdir(script_path)
-    pyproma_projectview.ProjectView()
+    PyProma_project_view.ProjectView()
