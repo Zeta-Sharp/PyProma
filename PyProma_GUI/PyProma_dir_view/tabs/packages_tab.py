4f477b56cb7792109d257e24541fe3821e0bcbc3
rikeidanshi <rikeidanshi@duck.com>
2024-09-21 00:10:05 +0900

    fixed a error "No module named pip install" when we install packages in packages tab

--- PyProma_GUI/PyProma_dir_view/tabs/packages_tab.py
+++ PyProma_GUI/PyProma_dir_view/tabs/packages_tab.py
@@ -69,11 +69,11 @@ class PackagesTab(tab_template.TabTemplate):
             venv_path = os.path.join(
                 self.main.dir_path, ".venv", "Scripts", "python.exe")
             if os.path.isfile(venv_path):
-                tool = self.command_combo.get()
+                tool = self.command_combo.get().split(" ")
                 package = self.command_text.get()
                 if package:
                     self.run_command_button.config(state=tk.DISABLED)
-                    self.run_command([venv_path, "-m", tool, package])
+                    self.run_command([venv_path, "-m", *tool, package])
                     self.run_command_button.config(state=tk.ACTIVE)
 
     def run_command(self, command: str | list):
@@ -84,7 +84,7 @@ class PackagesTab(tab_template.TabTemplate):
         """
         try:
             process = subprocess.Popen(
-                command, shell=True, text=True,
+                args=command, shell=True, text=True,
                 stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
             while True:
